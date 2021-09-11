431813c0d18e40bb74b9405a25a2bc149b95ead6
developergyu <keg1621@gmail.com>
2021-09-12 01:28:46 +0900

    종목 1개만 완성
    종목 1개 가격 및 퍼센트 가져옴

--- MainActivity.cs
+++ MainActivity.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
 using Android.App;
 using Android.OS;
 using Android.Runtime;
@@ -6,6 +9,7 @@ using Android.Support.Design.Widget;
 using Android.Support.V7.App;
 using Android.Views;
 using Android.Widget;
+using HtmlAgilityPack;
 
 namespace SearchStock
 {
@@ -23,6 +27,24 @@ namespace SearchStock
 
             FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
             fab.Click += FabOnClick;
+
+            TextView price = FindViewById<TextView>(Resource.Id.Textview_Price);
+            TextView percent = FindViewById<TextView>(Resource.Id.Textview_Percent);
+
+
+            WebClient wc = new WebClient();
+            wc.Encoding = Encoding.UTF8;
+            string html = wc.DownloadString("https://finance.naver.com/item/sise.nhn?code=005930");
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(html);
+
+            HtmlNode hn = doc.GetElementbyId("chart_area");
+
+            string str_price = hn.SelectSingleNode("div").SelectSingleNode("dl").SelectNodes("dd")[0].InnerText; //현재시세
+            string stf_percent = hn.SelectSingleNode("div").SelectSingleNode("dl").SelectNodes("dd")[2].InnerText; //몇퍼
+            price.Text = Regex.Replace(str_price, @"\D", "");
+            percent.Text = stf_percent;
+
         }
 
         public override bool OnCreateOptionsMenu(IMenu menu)
@@ -49,11 +71,11 @@ namespace SearchStock
                 .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
         }
 
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        //public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        //{
+        //    Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
+        //    base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        //}
 	}
 }
